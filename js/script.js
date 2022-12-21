fc184b05db773090c6dace2fa06a32988489ea38
MaheshLikhar <118125963+MaheshLikhar@users.noreply.github.com>
2022-12-21 12:03:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
 let loginForm = document.querySelector('.header .login-form');
 let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
-};
-
-document.querySelector('#search-btn').onclick = () =>{
-   searchForm.classList.toggle('active');
-   navbar.classList.remove('active');
    loginForm.classList.remove('active');
 };
 
